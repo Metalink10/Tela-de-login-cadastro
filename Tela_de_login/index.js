4c2f6f7e5cdf79c1f117aed730c30427d39c5ef2
Metalink10 <lmlira29@gmail.com>
2025-03-04 13:35:29 -0300

    Add files via upload

---
+++ Tela_de_login/index.js
@@ -0,0 +1,45 @@
+const form = document.getElementById("form");
+const btn = document.getElementById("btn");
+const loginNome = document.getElementById("nome");
+const loginSenha = document.getElementById("senha");
+const msg = document.getElementById("msgSucesso");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let valorNome = loginNome.value;
+  let valorSenha = loginSenha.value;
+
+  let listaUser = [];
+
+  let userValid = {
+    nome: "",
+    senha: "",
+  };
+
+  listaUser = JSON.parse(
+    localStorage.getItem(
+      "https://tela-de-cadastro-git-main-metalink10s-projects.vercel.app",
+      
+    )
+  );
+  console.log(listaUser)
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let value = localStorage.getItem(key);
+    console.log(key, value);
+  }
+
+  if (valorNome === "" && valorSenha === "") {
+    alert("Preencha os campos ou cadastre-se para acessar");
+  } else if (valorNome == userValid.nome && valorSenha == userValid.senha) {
+    window.localStorage
+    setTimeout(() => {
+      window.location.href = "https://cadastrar-tela-de-cadastro.vercel.app/";
+      msg.setAttribute("style", "display: block");
+    }, 2000);
+  } else {
+    alert("Cadastre-se para acessar");
+  }
+});
