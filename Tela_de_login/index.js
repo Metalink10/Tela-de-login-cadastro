8d9945a49c34358a137690cd5fa0b6073b97181c
Metalink10 <lmlira29@gmail.com>
2025-03-08 15:45:50 -0300

    Update index.js

--- Tela_de_login/index.js
+++ Tela_de_login/index.js
@@ -19,7 +19,7 @@ btn.addEventListener("click", (e) => {
 
    fetch(https://cadastre-se-git-main-metalink10s-projects.vercel.app/")
   .then(Response => Response.json())
-         console.log(Result)
+         console.log(Response)
   //.then(result => console.log(result));
   );
  
