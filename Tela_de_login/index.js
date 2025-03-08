f367e05c942be1ecf96281e7abd05568aa9e6a88
Metalink10 <lmlira29@gmail.com>
2025-03-08 15:17:38 -0300

    Update index.js

--- Tela_de_login/index.js
+++ Tela_de_login/index.js
@@ -21,23 +21,5 @@ btn.addEventListener("click", (e) => {
   .then(Response => Response.json())
   .then(result => console.log(result));
   );
-  console.log(listaUser)
-
-  for (let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i);
-    let value = localStorage.getItem(key);
-    console.log(key, value);
-  }
-
-  if (valorNome === "" && valorSenha === "") {
-    alert("Preencha os campos ou cadastre-se para acessar");
-  } else if (valorNome == userValid.nome && valorSenha == userValid.senha) {
-    window.localStorage
-    setTimeout(() => {
-      window.location.href = "https://cadastrar-tela-de-cadastro.vercel.app/";
-      msg.setAttribute("style", "display: block");
-    }, 2000);
-  } else {
-    alert("Cadastre-se para acessar");
-  }
+ 
 });
